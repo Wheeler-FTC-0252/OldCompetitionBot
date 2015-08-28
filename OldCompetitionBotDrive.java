56b07c7b41a8e6be8426d7db52c6e08c61ebc03c
Lucien Gaitskell <lucien@gaitskell.com>
2015-08-28 13:53:52 -0400

    added Drive code

---
+++ OldCompetitionBotDrive.java
@@ -0,0 +1,11 @@
+package org.wheeler.robotics.OldCompetitionBot;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by lucien on 8/28/15.
+ */
+public class OldCompetitionBotDrive extends OpMode {
+    public void init(){}
+    public void loop(){}
+}
