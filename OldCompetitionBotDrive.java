31b8a9ca69213ccb792592e4b3158a8249e14df6
Lucien Gaitskell <lucien@gaitskell.com>
2015-08-30 20:14:03 -0400

    finished drive

--- OldCompetitionBotDrive.java
+++ OldCompetitionBotDrive.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 /**
  * Created by lucien on 8/28/15.
@@ -19,11 +20,13 @@ public class OldCompetitionBotDrive extends OpMode {
     Servo doorServo;
     Servo leftSpinServo;
     Servo rightSpinServo;
+    TouchSensor armTouch;
     boolean buttonA;
     boolean oldButtonA=false;
+    double armSpeed;
     double leftTrigger;
     double rightTrigger;
-    double[] doorPositions = {0 , 0.5};
+    double[] doorPositions = {0 , 0.6};
     double[] grabPositions = {0 ,0.6};
     double leftX;
     double leftY;
@@ -53,6 +56,8 @@ public class OldCompetitionBotDrive extends OpMode {
         doorServo.setPosition(doorPositions[0]);
         grabServo.setPosition(grabPositions[1]);
         extraController=hardwareMap.dcMotorController.get("extra");
+        extraController.setMotorControllerDeviceMode(DcMotorController.DeviceMode.READ_WRITE);
+        armTouch=hardwareMap.touchSensor.get("armTouch");
     }
 
     public void loop(){
@@ -60,13 +65,13 @@ public class OldCompetitionBotDrive extends OpMode {
         leftY=gamepad1.left_stick_y;
         rightX=gamepad1.right_stick_x;
         rightY=gamepad1.right_stick_y;
-        leftTrigger=gamepad1.left_trigger;
-        rightTrigger=gamepad1.right_trigger;
+        leftTrigger=gamepad2.left_trigger;
+        rightTrigger=gamepad2.right_trigger;
         dpadUp=gamepad1.dpad_up;
         dpadDown=gamepad1.dpad_down;
         dpadLeft=gamepad1.dpad_left;
         dpadRight=gamepad1.dpad_right;
-        buttonA=gamepad1.a;
+        buttonA=gamepad2.a;
         leftY2=gamepad2.left_stick_y;
         frontLeftMotor.setPower(leftY);
         backLeftMotor.setPower(leftY);
@@ -74,7 +79,7 @@ public class OldCompetitionBotDrive extends OpMode {
         backRightMotor.setPower(rightY);
         telemetry.addData("Grab Servo: ", grabServo.getPosition());
         telemetry.addData("Door Servo: ", doorServo.getPosition());
-        telemetry.addData("arm pos: ", armMotor.getCurrentPosition());
+        //telemetry.addData("arm pos: ", armMotor.getCurrentPosition());
         leftSpinServo.setPosition(0.5+(leftTrigger/2));
         rightSpinServo.setPosition(0.5+(rightTrigger/2));
 
@@ -94,6 +99,13 @@ public class OldCompetitionBotDrive extends OpMode {
         else if (dpadDown){
             grabServo.setPosition(grabPositions[0]);
         }
-        armMotor.setPower(leftY2*armGain);
+
+        armSpeed=leftY2*armGain;
+        telemetry.addData("speed: ", armSpeed);
+        telemetry.addData("touch: ", armTouch.isPressed());
+        telemetry.addData("armEncoder: ", armMotor.getCurrentPosition());
+        if (armTouch.isPressed() && armSpeed>0) armSpeed=0;
+        armMotor.setPower(armSpeed);
+
     }
 }
